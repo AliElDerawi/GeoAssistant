41cbd12f48d0e2e2ccbcd458a504b83a54797571
Ali Al-Derawi <aalderawi2013@students.iugaza.edu.ps>
2024-11-26 12:03:09 +0200

    feat: Update Android navigation dependency to the last stable version.
    From v2.8.3 to v2.8.4.

--- app/src/main/java/com/udacity/project4/saveReminder/viewModel/SaveReminderViewModel.kt
+++ app/src/main/java/com/udacity/project4/saveReminder/viewModel/SaveReminderViewModel.kt
@@ -13,6 +13,7 @@ import com.google.android.gms.location.GeofencingClient
 import com.google.android.gms.location.GeofencingRequest
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.PointOfInterest
+import com.udacity.project4.BuildConfig
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseViewModel
 import com.udacity.project4.base.NavigationCommand
@@ -270,7 +271,9 @@ class SaveReminderViewModel(
         mGeofencingClient.removeGeofences(geofencePendingIntent).run {
             addOnSuccessListener {
                 Timber.d(mApp.getString(R.string.msg_geofences_removed))
-                showToastInt.value = R.string.msg_geofences_removed
+//                if (BuildConfig.DEBUG) {
+//                    showToastInt.value = R.string.msg_geofences_removed
+//                }
             }
             addOnFailureListener {
                 Timber.d(mApp.getString(R.string.msg_geofences_not_removed))
